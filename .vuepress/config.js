71b8417bb66c10bc173c8d2b1de2a12b9a649c8e
Suriel <mhecy98@gmail.com>
2019-11-27 15:40:24 +0800

    更改路径

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
   // 设置输出目录
   dest: './dist',
   // 设置站点根路径，如果你在访问的地址是 'www.xxxx.com/wxDocs' 那么就设置成 '/wxDocs/'
-  base: '/',
+  base: '/docs',
   // 添加 github 链接，但是这个要放在公司的内网服务器，所以为空
   // repo: '',
   //   head: [['link', { rel: 'icon', href: '/logo.ico' }]],
